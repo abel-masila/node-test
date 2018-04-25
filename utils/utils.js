46079502a99768a47583d4b1aee8bd51b18e9646
masila <abelmasila@gmail.com>
2018-04-25 12:44:53 +0300

    Added a test for square method

--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,2 @@
 module.exports.add=(a,b)=> a+b;
+module.exports.square=(x)=> x*x;
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,5 +2,13 @@ const utils=require('./utils');
 
 it('should add two numbers',()=>{
     const result=utils.add(30,60);
-    
+    if(result!==90){
+        throw new Error(`Expected 90, but got ${result}`);
+    }
+})
+it('should square a number',()=>{
+    const result=utils.square(2);
+    if(result!==4){
+        throw new Error(`Expected 4, but got ${result}`);
+    }
 })
