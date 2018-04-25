4b773ee2cd127fb6597e2cd9a9bbef5f9b559334
masila <abelmasila@gmail.com>
2018-04-25 15:34:50 +0300

    added describe blocks for the test

--- server/server.test.js
+++ server/server.test.js
@@ -3,32 +3,35 @@ const request=require('supertest');
 
  const expect=require('expect');
 
- it("should return hello world response",(done)=>{
-     request(app)
-        .get('/')
-        .expect((res)=>{
-            expect(res.body).toEqual({
-                error:'Page Not Found',
-                name:'Todo App v1.0'
-            })
-        })
-        .end(done);
- });
- it("should return users object",(done)=>{
-     request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res)=>{
-            expect(res.body).toEqual([
-                {
-                    name:'Abel',
-                    age:25
-                },
-                {
-                    name:"Andie",
-                    age:21
-                }
-            ])
-        })
-        .end(done)
- });
+ describe("server",()=>{
+    it("should return hello world response",(done)=>{
+        request(app)
+           .get('/')
+           .expect((res)=>{
+               expect(res.body).toEqual({
+                   error:'Page Not Found',
+                   name:'Todo App v1.0'
+               })
+           })
+           .end(done);
+    });
+    it("should return users object",(done)=>{
+        request(app)
+           .get('/users')
+           .expect(200)
+           .expect((res)=>{
+               expect(res.body).toEqual([
+                   {
+                       name:'Abel',
+                       age:25
+                   },
+                   {
+                       name:"Andie",
+                       age:21
+                   }
+               ])
+           })
+           .end(done)
+    });
+ })
+
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,30 +1,32 @@
 const utils=require('./utils');
 const expect=require('expect');
 
-it('should add two numbers',()=>{
-    const result=utils.add(30,60);
-    expect(result).toBe(90);
-    expect(typeof result).toBe('number');
-})
-it('should square a number',()=>{
-    const result=utils.square(2);
-    expect(result).toBe(4);
-    expect(typeof result).toBe('number');
-})
-it("should expect some values",()=>{
-    expect(12).not.toBe(11)
-})
-it('should async add two numbers',(done)=>{
-    utils.asyncAdd(3,4,(sum)=>{
-        expect(sum).toBe(7);
-        expect(typeof sum).toBe('number');
-        done();
+describe('Utils',()=>{
+    it('should add two numbers',()=>{
+        const result=utils.add(30,60);
+        expect(result).toBe(90);
+        expect(typeof result).toBe('number');
     })
-})
-it('should async square a number',(done)=>{
-    utils.asyncSquare(3, (square)=>{
-        expect(square).toBe(9);
-        expect(typeof square).toBe('number');
-        done();
+    it('should square a number',()=>{
+        const result=utils.square(2);
+        expect(result).toBe(4);
+        expect(typeof result).toBe('number');
+    })
+    it("should expect some values",()=>{
+        expect(12).not.toBe(11)
+    })
+    it('should async add two numbers',(done)=>{
+        utils.asyncAdd(3,4,(sum)=>{
+            expect(sum).toBe(7);
+            expect(typeof sum).toBe('number');
+            done();
+        })
+    })
+    it('should async square a number',(done)=>{
+        utils.asyncSquare(3, (square)=>{
+            expect(square).toBe(9);
+            expect(typeof square).toBe('number');
+            done();
+        })
     })
 })
