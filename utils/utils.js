6652968e5465ff855259afd7904d5cf1bc392941
masila <abelmasila@gmail.com>
2018-04-25 14:45:27 +0300

    Async method tests

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,13 @@
 module.exports.add=(a,b)=> a+b;
-module.exports.square=(x)=> x*x ;
+module.exports.square=(x)=> x*x;
+
+module.exports.asyncAdd=(a,b,callback)=>{
+    setTimeout(() => {
+        callback(a+b);
+    }, 1000);
+}
+module.exports.asyncSquare=(x, callback)=>{
+    setTimeout(() => {
+        callback(x*x)
+    }, 1000);
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,14 +1,30 @@
 const utils=require('./utils');
+const expect=require('expect');
 
 it('should add two numbers',()=>{
     const result=utils.add(30,60);
-    if(result!==90){
-        throw new Error(`Expected 90, but got ${result}`);
-    }
+    expect(result).toBe(90);
+    expect(typeof result).toBe('number');
 })
 it('should square a number',()=>{
     const result=utils.square(2);
-    if(result!==4){
-        throw new Error(`Expected 4, but got ${result}`);
-    }
+    expect(result).toBe(4);
+    expect(typeof result).toBe('number');
+})
+it("should expect some values",()=>{
+    expect(12).not.toBe(11)
+})
+it('should async add two numbers',(done)=>{
+    utils.asyncAdd(3,4,(sum)=>{
+        expect(sum).toBe(7);
+        expect(typeof sum).toBe('number');
+        done();
+    })
+})
+it('should async square a number',(done)=>{
+    utils.asyncSquare(3, (square)=>{
+        expect(square).toBe(9);
+        expect(typeof square).toBe('number');
+        done();
+    })
 })
