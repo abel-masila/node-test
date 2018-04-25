c9ebe5da04b788c04dd42d40d82f48d72799221f
masila <abelmasila@gmail.com>
2018-04-25 15:28:11 +0300

    Added express test

--- server/server.js
+++ server/server.js
@@ -3,7 +3,16 @@ const express= require('express');
 const app= express();
 
 app.get('/',(req,res)=>{
-    res.send('Hello world');
+res.send({ error:'Page Not Found', name: 'Todo App v1.0'});
+});
+app.get('/users',(req,res)=>{
+    res.send([{
+        name:"Abel",
+        age:25
+    },{
+        name:"Andie",
+        age:21
+    }]);
 });
 
 app.listen(3000);
